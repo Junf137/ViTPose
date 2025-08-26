645f7a54fac798a5abc8c631a446f0732b395bda
Junfeng Lei <junf137@outlook.com>
2025-08-26 18:46:00 +0000

    feat: use body coco in batch process

--- batch_process_vid_masks.sh
+++ batch_process_vid_masks.sh
@@ -2,7 +2,7 @@
 
 video_root="/root/Videos/challenging_videos"
 mask_root="/root/Videos/sam2_carego_mask"
-output_root="/root/Videos/output/mask"
+output_root="/root/Videos/output/vitpose_huge_body_coco"
 
 
 for video_path in $video_root/*.mp4; do
@@ -14,8 +14,8 @@ for video_path in $video_root/*.mp4; do
     echo "Output path: $output_root"
 
     python demo/top_down_video_demo_with_mask.py \
-        configs/wholebody/2d_kpt_sview_rgb_img/topdown_heatmap/coco-wholebody/ViTPose_huge_wholebody_256x192.py \
-        models/split/wholebody.pth  \
+        configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/ViTPose_huge_coco_256x192.py \
+        models/split/coco.pth  \
         --video-path $video_path \
         --mask-video-path $mask_path \
         --out-video-root $output_root

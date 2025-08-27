c9459218161dea5736425b836b52be0c47ef1d8d
Junfeng Lei <junf137@outlook.com>
2025-08-27 22:48:49 +0000

    feat: batch output pose + using vitpose instead of plus

--- batch_process_vid_masks.sh
+++ batch_process_vid_masks.sh
@@ -3,7 +3,9 @@
 video_root="/root/Videos/challenging_videos"
 mask_root="/root/Videos/sam2_carego_mask"
 output_root="/root/Videos/output/vitpose_huge_body_coco"
-
+mask_bbox_video_root="root/Videos/output/vitpose_huge_body_coco"
+bbox_output_root="output/bbox"
+pose_output_root="output/pose"
 
 for video_path in $video_root/*.mp4; do
     video_name=$(basename $video_path)
@@ -15,8 +17,11 @@ for video_path in $video_root/*.mp4; do
 
     python demo/top_down_video_demo_with_mask.py \
         configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/ViTPose_huge_coco_256x192.py \
-        models/split/coco.pth  \
+        models/vitpose-h-multi-coco.pth \
         --video-path $video_path \
         --mask-video-path $mask_path \
-        --out-video-root $output_root
+        --out-video-root $output_root \
+        --bbox-output-root $bbox_output_root \
+        --pose-output-root $pose_output_root \
+        --mask-bbox-video-root $mask_bbox_video_root
 done
